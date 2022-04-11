9fe18d5caa03f5812f0d4ca807b9872b553a34ac
matte887 <m.erroi92@gmail.com>
2022-04-11 15:55:53 +0200

    Created folder for bonus 1

---
+++ Bonus-1/js/script.js
@@ -0,0 +1,19 @@
+// Stampare in console numeri da 1 a 100
+
+for (let i = 1; i <= 100; i++) {
+    // Creare una variabile a cui assegnare a seconda dei casi Fizz o Buzz o FizzBuzz
+    let FizzBuzz;
+    if (i % 3 === 0 && i % 5 === 0) {
+        FizzBuzz = "fizz-buzz";
+    } else if (i % 3 === 0) {
+        FizzBuzz = "fizz"; 
+    } else if (i % 5 === 0) {
+        FizzBuzz = "buzz";
+    } else {
+        FizzBuzz = "bg-blue";
+    }
+    console.log(i, FizzBuzz);
+
+    // Appendere un elemento HTML ad ogni i
+    document.querySelector(".box-wrapper").innerHTML += `<div class="box ${FizzBuzz}">${i}</div>`;
+}
--- Consegna-base/js/script.js
+++ Consegna-base/js/script.js
@@ -1,13 +1,14 @@
 // Stampare in console numeri da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
+    // Creare una variabile a cui assegnare a seconda dei casi Fizz o Buzz o FizzBuzz
     let FizzBuzz;
     if (i % 3 === 0 && i % 5 === 0) {
-        FizzBuzz = "FizzBuzz";
+        FizzBuzz = "fizz-buzz";
     } else if (i % 3 === 0) {
-        FizzBuzz = "Fizz"; 
+        FizzBuzz = "fizz"; 
     } else if (i % 5 === 0) {
-        FizzBuzz = "Buzz";
+        FizzBuzz = "buzz";
     }
     console.log(i, FizzBuzz);
 }
