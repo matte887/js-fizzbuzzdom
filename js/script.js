e19e7193638e2f63e4092e6022ebc6145970c7eb
matte887 <m.erroi92@gmail.com>
2022-04-11 14:24:59 +0200

    Logic working correctly

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert ("ciao");
+// Stampare in console numeri da 1 a 100
+
+for (let i = 1; i <= 100; i++) {
+    let FizzBuzz;
+    if (i % 3 === 0 && i % 5 === 0) {
+        FizzBuzz = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        FizzBuzz = "Fizz"; 
+    } else if (i % 5 === 0) {
+        FizzBuzz = "Buzz";
+    }
+    console.log(i, FizzBuzz);
+}
