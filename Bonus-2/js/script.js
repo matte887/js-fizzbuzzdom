b9e98be2546b837d6298c98c05b77b17e36c8fe1
matte887 <m.erroi92@gmail.com>
2022-04-12 10:31:46 +0200

    Fix bonus 2

--- Bonus-2/js/script.js
+++ Bonus-2/js/script.js
@@ -1,23 +1,25 @@
-// Stampare in console numeri da 1 a 100
+const rowBox = document.querySelector(".box-wrapper");
 
+// Stampare in console numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     // Creare una variabile a cui assegnare a seconda dei casi Fizz o Buzz o FizzBuzz
     let FizzBuzz;
+    let color = "";
     if (i % 3 === 0 && i % 5 === 0) {
         FizzBuzz = "fizzbuzz";
+        color = "bg-red";
     } else if (i % 3 === 0) {
         FizzBuzz = "fizz"; 
+        color = "bg-green";
     } else if (i % 5 === 0) {
         FizzBuzz = "buzz";
+        color = "bg-yellow";
     } else {
-        FizzBuzz = "bg-blue";
+        FizzBuzz = i;
+        color = "bg-blue";
     }
     console.log(i, FizzBuzz);
-
-    // Appendere un elemento HTML ad ogni i
-    if (FizzBuzz === "fizzbuzz" || FizzBuzz === "fizz" || FizzBuzz === "buzz") {
-        document.querySelector(".box-wrapper").innerHTML += `<div class="box ${FizzBuzz}">${FizzBuzz}</div>`;
-    } else {
-        document.querySelector(".box-wrapper").innerHTML += `<div class="box ${FizzBuzz}">${i}</div>`;
-    }
+    
+    // Appendere un elemento HTML ad ogni i ed assegnare una classe
+    rowBox.innerHTML += `<div class="box ${color}">${FizzBuzz}</div>`;
 }
