0d8619cd579ef49d470feb34be6f9845cb180613
matte887 <m.erroi92@gmail.com>
2022-04-12 10:17:37 +0200

    Fix su bonus-1

--- Bonus-1/js/script.js
+++ Bonus-1/js/script.js
@@ -1,5 +1,5 @@
 // Stampare in console numeri da 1 a 100
-
+const rowBox = document.querySelector(".box-wrapper");
 for (let i = 1; i <= 100; i++) {
     // Creare una variabile a cui assegnare a seconda dei casi Fizz o Buzz o FizzBuzz
     let FizzBuzz;
@@ -9,13 +9,10 @@ for (let i = 1; i <= 100; i++) {
         FizzBuzz = "fizz"; 
     } else if (i % 5 === 0) {
         FizzBuzz = "buzz";
-    }
-    console.log(i, FizzBuzz);
+    } else FizzBuzz = i;
+
+    console.log(FizzBuzz);
 
     // Appendere un elemento HTML ad ogni i
-    if (FizzBuzz === "fizz-buzz" || FizzBuzz === "fizz" || FizzBuzz === "buzz") {
-        document.querySelector(".box-wrapper").innerHTML += `<div class="box">${FizzBuzz}</div>`;
-    } else {
-        document.querySelector(".box-wrapper").innerHTML += `<div class="box">${i}</div>`;
-    }
+    rowBox.innerHTML += `<div class="box">${FizzBuzz}</div>`;
 }
